461a8f8c0e1578da69340580e03f3cd5d5a86428
dennisorion <dennis.orion@hotmail.com>
2024-07-19 12:05:00 +0000

    Atual

--- app2.py
+++ app2.py
@@ -3,69 +3,26 @@ from markupsafe import escape
 
 app2 = Flask(__name__)
 
+
 usuarios = [
-    {
-        'nome': "Dennis",
-        'idade': 31,
-        'profissao': "psicologo",
-    },
     {
         'nome': "Amanda",
         'idade': 37,
-        'profissao': "pedagoga",
+        'profissao': "psi",
     },
     {
-        'nome': "Rosa",
-        'idade': 93,
-        'profissao': "aposentada"
-    ,}
-    ]
+        'nome': 'Dennis',
+        'idade': 31,
+        'profissao': 'pedagogo',
+    }
+]
 
-@app2.route('/')
+app2.route('/main')
 def main():
-    return "olá"
+    return '<h1>Ai sim porra</h1>'
 
-@app2.route('/cadastro')
-def cadastro():
-    return "aqui fica teu currículo"
 
-@app2.route('/abominacao')
-def abominacao():
-    return '<h1> minha primeira pagina </h1>' 
 
-@app2.route('/bd')
-def bd():
-    return "<p1> Aqui fuca o banco de dados</p1>"
-
-@app2.route('/formulario')
-def formulario():
-
-        form = '''
-        <form>
-            <label>Primeira nome:</label)
-            <input type="text"><br><br>
-            <label>idade:</label>
-            <input type="text"><br><br>
-            <input type="submit" value="enviar"
-        </form>
-'''
-        return form
-
-@app2.route('/v1/user/idade/<nome>', methods=["GET"])
-def consulta(nome):
-     for usuario in usuarios:
-          if usuario ['nome'] == nome:
-               return jsonify(idade=usuario['idade'])
-     return jsonify(idade="usuario não encontrado"), 404
-
-@app2.route('/v2/user/consulta/', methods=['GET'])
-def consulta_usuario():
-    nome = request.args.get('nome')
-    profissao = request.args.get('profissao')
-    for usuario in usuarios:
-        if usuario ['nome'] == nome or usuario['profissao'] == profissao:
-            return jsonify(usuario)
-    return jsonify(mensagem='usuario não encontrao'), 404
-               
 if __name__ == "__main__":
     app2.run(debug=True)
+
